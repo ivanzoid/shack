874b7fefbc43a267c499665d7b1ffcf2d2e39c7a
Ivan Zezyulya <ivan@zezyulya.com>
2012-09-09 03:41:21 +0700

    Initial commit

---
+++ shack.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"bytes"
+	"encoding/xml"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+)
+
+const (
+	URL = "http://www.imageshack.us/upload_api.php"
+	KEY = "9ADLMQRY7964837ec095baa8a90c6ff47db45121"
+)
+
+func Die(format string, a ...interface{}) {
+	str := fmt.Sprintf(format, a)
+	fmt.Fprintf(os.Stderr, "%v\n", str)
+	os.Exit(1)
+}
+
+func FakeUTF8CharsetReader(charset string, input io.Reader) (io.Reader, error) {
+	return input, nil
+}
+
+func main() {
+	if len(KEY) == 0 {
+		Die("please set your API key in source")
+	}
+
+	// Get arguments
+
+	if len(os.Args) == 1 {
+		Die("missing file name\n")
+	}
+
+	filename := os.Args[1]
+
+	// Create form request
+
+	buffer := new(bytes.Buffer)
+	formWriter := multipart.NewWriter(buffer)
+
+	formWriter.WriteField("rembar", "1") // remove information bar
+	formWriter.WriteField("key", KEY)
+	fileWriter, _ := formWriter.CreateFormFile("fileupload", filename)
+
+	file, error := os.Open(filename)
+
+	if error != nil {
+		Die("can't open file: %v", error)
+	}
+
+	defer file.Close()
+
+	_, error = io.Copy(fileWriter, file)
+
+	if error != nil {
+		Die("can't read file: %v", error)
+	}
+
+	formWriter.Close()
+
+	request, error := http.NewRequest("POST", URL, buffer)
+
+	if error != nil {
+		Die("can't create request: %v", error)
+	}
+
+	request.Header.Set("Content-Type", formWriter.FormDataContentType())
+
+	// Create http client and perform request
+
+	client := new(http.Client)
+	response, error := client.Do(request)
+
+	if error != nil {
+		Die("error performing request: %v", error)
+	}
+
+	if response.StatusCode < 200 || response.StatusCode >= 300 {
+		Die("error: received %v status code", response.StatusCode)
+	}
+
+	// Parse result
+
+	type XmlResult struct {
+		Link string `xml:"links>image_link"`
+	}
+	result := XmlResult{}
+
+	xmlDecoder := xml.NewDecoder(response.Body)
+	xmlDecoder.CharsetReader = FakeUTF8CharsetReader
+	error = xmlDecoder.Decode(&result)
+
+	if error != nil {
+		Die("error parsing xml: %v", error)
+	}
+
+	fmt.Printf("%v", result.Link)
+}
